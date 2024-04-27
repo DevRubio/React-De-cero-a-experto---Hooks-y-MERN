3581eca69c257fcc92f06eea83495ab03a1692f2
Oscar Rubio <ofrubio0@gmail.com>
2024-04-26 21:39:35 -0500

    (ADD) Se crea proyecto counter-app-vite

---
+++ counter-app-vite/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from "react-dom/client"
+import {FirstApp} from './HelloWordApp'
+import './styles.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <FirstApp title='Hola' subtitle={123}/>
+    </React.StrictMode>
+)
