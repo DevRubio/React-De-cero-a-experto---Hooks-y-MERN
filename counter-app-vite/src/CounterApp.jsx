402a1378c3bee0cafb5a65032490e956dfec77c4
Oscar Rubio <ofrubio0@gmail.com>
2024-05-02 09:13:10 -0500

    (UPDATE) Se utiliza el hook useState

---
+++ counter-app-vite/src/CounterApp.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react';
+
+export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value)
+
+    const handelAdd = ()=>{
+        setCounter(counter + 1)
+    }
+
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <h2>{counter}</h2>
+      <button onClick={handelAdd}>+1</button>
+    </>
+  );
+};
+
+CounterApp.propTypes={
+    value: PropTypes.number.isRequired
+}
--- counter-app-vite/src/main.jsx
+++ counter-app-vite/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from "react-dom/client"
 import {FirstApp} from './FirstApp'
 import './styles.css'
+import { CounterApp } from './CounterApp'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp/>
+        <CounterApp value={12}/>
     </React.StrictMode>
 )
