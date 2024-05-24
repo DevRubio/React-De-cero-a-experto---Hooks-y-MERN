bc2ead97032cae09cf83a671fe559ed0d09a88a1
Oscar Rubio <ofrubio0@gmail.com>
2024-05-23 19:07:41 -0500

    (UPDATE) Se agrengan pruebas de eventos al componente counterApp

--- counter-app-vite/tests/CounterApp.test.jsx
+++ counter-app-vite/tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import { CounterApp } from '../src/CounterApp'
 
 describe('Pruebas en el <CounterApp/>', () => { 
@@ -12,4 +12,25 @@ describe('Pruebas en el <CounterApp/>', () => {
         render(<CounterApp value={100}/>)
         expect(screen.getAllByText('100')).toBeTruthy()
      })
+
+     test('debe de incrementar con el boton +1', () => { 
+        render(<CounterApp value={initialValue}/>)
+        fireEvent.click(screen.getByText('+1'))
+        expect(screen.getByText('11')).toBeTruthy()
+      })
+
+      test('debe de decrementar con el boton de -1', () => { 
+        render(<CounterApp value={initialValue}/>)
+        fireEvent.click(screen.getByText('-1'))
+        expect(screen.getByText('9')).toBeTruthy()
+       })
+
+       test('debe de funcionar el boton de reset', () => { 
+        render(<CounterApp value={10}/>)
+        fireEvent.click(screen.getByText('+1'))
+        //fireEvent.click(screen.getByText('Reset'))
+        //expect(screen.getByText('10')).toBeTruthy()
+        fireEvent.click(screen.getByRole('button',{name: 'btn-reset'}))
+        expect(screen.getByText('10')).toBeTruthy()
+       })
  })
