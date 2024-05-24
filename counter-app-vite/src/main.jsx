04589d465ebefcdacf8024cee0e278d0037708ad
Oscar Rubio <ofrubio0@gmail.com>
2024-05-23 20:00:28 -0500

    (ADD) Se agrega el componente FirstApp al main

--- counter-app-vite/src/main.jsx
+++ counter-app-vite/src/main.jsx
@@ -7,6 +7,7 @@ import { CounterApp } from './CounterApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp value={12}/>
+        <FirstApp title='Hola, Como estan?'/>
+        <CounterApp value={12}/>        
     </React.StrictMode>
 )
