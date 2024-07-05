c59c6b0d47c14bda653e7aa0bde88b7e028a7739
ofrubio0 <ofrubio0@gmail.com>
2024-07-05 17:58:30 +0000

    (ADD) Se crea el componente GifItem.jsx y se exporta en GifGrid.jsx

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,12 +1,34 @@
+import { useState, useEffect } from "react"
 import { getGifs } from "../helpers/getGifs"
+import { GifItem } from "./GifItem"
 
 export const GifGrid = ({category}) => {
+  const [images, setImages] = useState([])
 
-  getGifs(category)
+  const getImages = async () =>{
+    const gifs = await getGifs(category)
+    setImages(gifs)
+  }
+
+  useEffect( () => {
+    getImages()
+  },[])
+
 
   return (
     <>
         <h3>{category}</h3>
+
+        <div className="card-grid">
+          {
+            images.map( (image) => (
+             <GifItem 
+              key={image.id}
+              {...image}  
+            />
+            ))
+          }
+        </div>
     </>
   )
 }
