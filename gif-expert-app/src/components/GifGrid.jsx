d629e36e3105f48d439cfe63132b64e2b482d02c
ofrubio0 <ofrubio0@gmail.com>
2024-07-05 18:22:22 +0000

    (UPDATE) Se agrega condicion del isLoading

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -9,6 +9,10 @@ export const GifGrid = ({category}) => {
     <>
         <h3>{category}</h3>
 
+        {
+          isLoading && (<h>Cargando...</h>)
+        }
+
         <div className="card-grid">
           {
             images.map( (image) => (
