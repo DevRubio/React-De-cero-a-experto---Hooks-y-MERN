aa5589b22974550f82853f7414bca5df18ab6a2b
ofrubio0 <ofrubio0@gmail.com>
2024-07-18 17:20:44 +0000

    (ADD) Se crea el componente SimpleForm para hacer uso del useEffect

---
+++ hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react"
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        username: 'Rubio',
+        email: 'notiene@google.com',
+    })
+
+    const { username, email } = formState
+
+    const handleInputChange = ({ target }) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        })
+    }
+
+    useEffect(() => {
+        console.log('hey')
+    }, [])
+
+    useEffect(() => {
+        console.log('formState Cambio')
+    }, [formState])
+
+    return (
+        <>
+            <h1>Formulario simple </h1>
+            <hr />
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="UserName"
+                name="username"
+                value={username}
+                onChange={handleInputChange}
+            />
+
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="UserName@mail.com"
+                name="email"
+                value={email}
+                onChange={handleInputChange}
+            />
+
+        </>
+    )
+}
